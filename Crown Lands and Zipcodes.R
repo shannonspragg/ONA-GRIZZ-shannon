1dc09d6e72f53b24b8eda1b62f5a9503fa9b5267
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-09-07 13:08:49 -0600

    finally figured out how to get the st_intersection overlap processed!

--- Crown Lands and Zipcodes.R
+++ Crown Lands and Zipcodes.R
@@ -25,43 +25,57 @@ plot(st_geometry(bc.zips))
 st_geometry(bc.zips) #This is WGS84 CRS
 
 #Need to reproject the crown lands to match the survey data:
-crown.reproj <- st_transform(bc.crown.lands.shp, st_crs(bc.zips))
+# crown.reproj <- st_transform(bc.crown.lands.shp, st_crs(bc.zips))
 st_crs(crown.reproj)
 
+# Lets actually try matching the zips to the crown lands:
+zips.reproj <- st_transform(bc.zips, st_crs(bc.crown.lands.shp))
+st_crs(zips.reproj)
+
 #make sure your coordinate systems projections are equal.
-st_crs(crown.reproj) == st_crs(bc.zips)
+ st_crs(zips.reproj) == st_crs(bc.crown.lands.shp)
 # This returns TRUE so we are good
 
 # Finding where the Crown Lands & Zipcodes Intersect ----------------------
 #Trying to find the proportion of crown land in each postal code and assign that to each respondent
-
+ ??st_intersection()
+ # st_is_valid(st_make_valid(st_set_precision(bc.zips, 1e6)))     https://github.com/r-spatial/sf/issues/1710
+ #This doesn't do what I want it to...
+ # sf_use_s2(FALSE)        https://stackoverflow.com/questions/68478179/how-to-resolve-spherical-geometry-failures-when-joining-spatial-data
+ #This just turns off the spherical geometry
+ crown.valid<-st_make_valid(bc.crown.lands.shp)
+ str(crown.valid)
+ st_is_valid(zips.reproj)
+ bc.zips.valid<- st_make_valid(zips.reproj)
+ str(bc.zips.valid)
+ 
 # Lets try this on a smaller chunk of the zipcodes data to see if it works ok:
-zips.mini<- bc.zips[1:5, ]
+zips.mini<- bc.zips.valid[1:5, ]
 # crown.mini <- crown.reproj[1:5, ] # Don't need this at all
 str(zips.mini)
 st_crs(zips.mini)
 
-zip.mini.reproj <- st_transform(zips.mini, st_crs(crown.reproj))
+# zip.mini.reproj <- st_transform(zips.mini, st_crs(crown.reproj)) # We don't need this, we reprojected before the subset
 
+intersect_mini <- st_intersection(crown.valid, zips.mini) 
+# This is returning an error: attribute variables are assumed to be spatially constant throughout all geometries 
 
-intersect_mini <- st_intersection(crown.reproj$geometry, zips.mini$geometry) 
+intersect_full<- st_intersection(st_geometry(crown.valid), st_geometry(bc.zips.valid))
+# Now this seemed to work... lets check it
 
-??st_intersection()
-# st_is_valid(st_make_valid(st_set_precision(bc.zips, 1e6)))     https://github.com/r-spatial/sf/issues/1710
-#This doesn't do what I want it to...
-# sf_use_s2(FALSE)        https://stackoverflow.com/questions/68478179/how-to-resolve-spherical-geometry-failures-when-joining-spatial-data
-#This just turns off the spherical geometry
-crown.valid<-st_make_valid(crown.reproj)
-str(crown.valid)
-bc.zips.valid<- st_make_valid(bc.zips)
-str(bc.zips.valid)
+plot(st_geometry(intersect_full))
+# I THINK THIS WORKED?!
+plot(st_geometry(bc.zips.valid))
+plot(st_geometry(crown.valid))
 
-??st_intersection
-intersect_bcz <- st_intersection(crown.valid, bc.zips.valid)
+# Trying to Plot these on top of each other, with color assigned to overlap:
+plot(st_geometry(crown.valid), col= 'grey', border='grey')
+plot(st_geometry(intersect_full), col= 'red', add=TRUE)
+# HECK YES, we can see the overlap areas in red!!
 
-intersect_bcz <- st_intersection(crown.valid, bc.zips.valid)  %>%
- dplyr::mutate(intersect_area = st_area(.)) %>%   # create new column with shape area  
-  dplyr::select(NAME, intersect_area)   # only select columns needed to merge
+#intersect_bcz <- st_intersection(crown.valid, bc.zips.valid)  %>%
+# dplyr::mutate(intersect_area = st_area(.)) %>%   # create new column with shape area  
+#  dplyr::select(NAME, intersect_area)   # only select columns needed to merge
 
 # The "Evaluation error: Found 2 features with invalid spherical geometry" suggests that some of your polygon geometries 
 # are invalid (possibly because they were digitized badly in the first place)

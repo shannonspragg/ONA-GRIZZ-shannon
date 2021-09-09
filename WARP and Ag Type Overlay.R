36b2eec989389805d944730a1eae02a3746c1b3c
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-09-09 13:45:26 -0600

    finishing up assigning dominant and total farm types to warp points!!

--- WARP and Ag Type Overlay.R
+++ WARP and Ag Type Overlay.R
@@ -31,24 +31,51 @@ bears.spdf<-SpatialPointsDataFrame(coords = xy,data = bc.sp,
 str(bears.spdf)
 
 bears.sf <- as(bears.spdf, "sf")
+str(bears.sf)
+# Write this into a .shp for future ease of importing!
+st_write(bears.sf, "WARP Bears Only Full.shp") #These map well!
+
+
+# FUTURE USE: Importing WARP Bears Full .shp ------------------------------
+# Here we practice importing it and make sure the data is all there:
+bears.full <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP Bears Only Full.shp")
+str(bears.full)
+head(bears.full) # This looks good, reads in as an sf data.frame!
 
 # Load in Canada Spatial Data ---------------------------------------------
-# This is where we bring in the metro areas shapefile
+# This is where we bring in the farm types shapefile
 
 # I found the .shp for BC Metro Areas! Read in Below:
-bc.dom.farms<-st_read("/Users/shannonspragg/ONA_GRIZZ/Ag census/Ag Census Dominant Farm Type /Dominant and Total Farm Types CCS.shp")
+bc.dom.farms<-st_read("/Users/shannonspragg/ONA_GRIZZ/Ag census/Ag Census Dominant Farm Type /Dominant Farm Type by CCS/Dominant Farm Types by CCS.shp")
 
 # Make sf and filter down to only British Columbia for Census SubDivs (CCS):
 bc.dom.farms.sf<- as(bc.dom.farms, "sf")
 str(bc.dom.farms.sf)
-head(bc.dom.farms.sf)
-st_crs(bc.dom.farms.sf)
-st_crs(bears.sf)
-# Re-project the Data ------------------------------------------------------
 
-# Canada_Albers_Equal_Area_Conic: Now we have the Ag Census areas projected to match the bears data
-bears.reproj.farms <- st_transform(bears.sf, st_crs(bc.dom.farms))
-plot(st_geometry(bc.dom.farms))
-st_crs(bears.reproj.farms)
-bc.met.reproj<- st_transform(bc.metro.sf, st_crs(bears.reproj.full))
-plot(st_geometry(bc.met.reproj))
+# Re-project the Data ------------------------------------------------------
+albers.crs <- CRS("+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=NAD83
+# +units=m +no_defs")
+st_crs(albers.crs) # Let's Match the data.frames to this CRS
+
+# Now we have the Ag Census areas projected to match the bears data
+bears.reproj <- st_transform(bears.full, st_crs(albers.crs))
+plot(st_geometry(bears.reproj))
+farms.reproj <- st_transform(bc.dom.farms.sf, st_crs(albers.crs))
+plot(st_geometry(farms.reproj))
+
+# Check to see if they match:
+st_crs(bears.reproj) == st_crs(farms.reproj) # [TRUE] = These ARE now the same
+
+# Spatial Join: WARP Points to Farm Type Polygon Attributes ---------------
+# For WARP POINTS that fall within CCS REGIONS, adds FARM TYPE ATTRIBUTES, retains ALL pts if left=TRUE, otherwise uses inner_join
+bears.farm.join <- st_join(bears.reproj, left = FALSE, farms.reproj["N_A_I_C"]) # join points
+head(bears.farm.join) # HECK TO THE YES - we successfully assigned points to a farm type category
+
+# Let's Plot this:
+plot(farms.reproj$geometry, border="gray20", col=NA)
+plot(bears.reproj$geometry, pch=21, cex=0.7, col="purple", bg="gray80", add=T)
+# There we go, nice points overlayed with the CCS polygons!
+
+# Write Farm Type Join into .shp: -----------------------------------------
+st_write(bears.farm.join, "/Users/shannonspragg/ONA_GRIZZ/Ag census/Ag Census Dominant Farm Type /Dominant Farm Type by CCS/WARP Dominant Farm Type Join.shp")
+

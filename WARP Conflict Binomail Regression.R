814fb16b5973b5fd64e8b6c5bc4521c4f740d797
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-09-28 15:28:49 -0600

    cleaning this up

--- WARP Conflict Binomail Regression.R
+++ WARP Conflict Binomail Regression.R
@@ -10,7 +10,7 @@
 warp.all.sp <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP All Species Full Yr/ WARP All Species Master Data Frame.shp")
 
 # Create Binomial GLM -- Dist to PA Variable -----------------------------------------------------
-<<<<<<< HEAD
+
 b2pa.distance <- scale(warp.all.sp$ds__PA_)
 bears_presence <- warp.all.sp$bears
 
@@ -26,7 +26,7 @@ y_sim <- rbinom(length(bears_presence), 1, prob = p)
 glm(y_sim ~ b2pa.distance, family = "binomial")
 
 # Example from Class Project:
-=======
+
 b2pa.distance <- warp.all.sp$ds__PA_
 bears_presence <- warp.all.sp$bears
 
@@ -34,12 +34,10 @@ Intercept=10
 Slope=30
 
 p=plogis(Intercept+Slope*b2pa.distance)
->>>>>>> c90dfc9451abe0becf3e42c4ae48007617546c91
 b.lik=dbinom(x=bears_presence,size = 1,prob = p,log = T)
 b.lik_sum=sum(b.lik)
 print(b.lik_sum)
 plot(jitter(bears_presence)~b2pa.distance, xlim=c(0,20),xlab="Distance to Nearest Protected Area (km)",ylab="Reported Conflict Point (Bear = 1, Other = 0)")
-<<<<<<< HEAD
 B1 <- glm(bears_presence~b2pa.distance,family="binomial") # Fit the other variables in here, to run them together in this model
 
 

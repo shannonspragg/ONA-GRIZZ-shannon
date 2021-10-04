0a41424c42d2ad49ddcb6f7bf9a4f53860eb22ed
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-10-04 11:58:30 -0600

    continued work on binomial regression

--- WARP Conflict Binomail Regression.R
+++ WARP Conflict Binomail Regression.R
@@ -1,29 +1,41 @@
 # WARP Conflict Binomial Linear Regression ------------------------------------------------
 # This script will be where I import the all species master df and configure it into the binomial 
 # linear regression analysis code
-  # Important Questions -- how do I determine what slope & intercept makes sense??
-  # Do I preform the binomial regression for each variable individually? or all together in the code?
+  # Important Questions -- NA
 
+
+# Load Packages -----------------------------------------------------------
+library(sf)
+library(tidyverse)
+library(dplyr)
 
 # Import the All Species Master df ----------------------------------------
 
 warp.all.sp <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP All Species Full Yr/ WARP All Species Master Data Frame.shp")
 
-# Create Binomial GLM -- Dist to PA Variable -----------------------------------------------------
+# Create Binomial GLM -- Bring in All Covariates -----------------------------------------------------
+bears_presence <- warp.all.sp$bears # Binomial bears
+b2pa.distance <- scale(warp.all.sp$ds__PA_) # Dist to PA covariate
+dom.farms <- warp.all.sp$Dmn_F_T # Dominant farm type covariate
+total.farms <- warp.all.sp$Ttl_F_C # Total farm count covariate
+b2met.dist <- warp.all.sp$dstn___ # Dist to metro covariate
 
-b2pa.distance <- scale(warp.all.sp$ds__PA_)
-bears_presence <- warp.all.sp$bears
 
+# Prep Simulation to Match Data with Binomial Reg: ------------------------
 # Creating a simulation to see if it returns close to the intercept and slope we input initially 
 # https://daviddalpiaz.github.io/appliedstats/logistic-regression.html
+
 Intercept=0.3 # This is set for a 0-1 value, representing bear presence
-Slope=2 # need a slope for each variable, create a matrix for each variable 
+Slope=2 # need a slope for each variable, so we create a matrix for each variable 
     # Do this like this: slope (B) <- runif(15,-2,2) https://data.library.virginia.edu/simulating-a-logistic-regression-model/
+B_slope <- runif(10, -2,2) # Created a randomized slope matrix for the variables
 
-p=plogis(Intercept+Slope*b2pa.distance) # simulating p, to fit model to p
+p=plogis(Intercept+Slope*b2pa.distance) # simulating p with our intercept and slope, to fit model to p
 
-y_sim <- rbinom(length(bears_presence), 1, prob = p)
-glm(y_sim ~ b2pa.distance, family = "binomial")
+y_sim <- rbinom(length(bears_presence), 1, prob = p) # running the simulation with probability p
+glm(y_sim ~ b2pa.distance, family = "binomial") # running the regression based on the simulation
+
+
 
 # Example from Class Project:
 
@@ -37,7 +49,7 @@ p=plogis(Intercept+Slope*b2pa.distance)
 b.lik=dbinom(x=bears_presence,size = 1,prob = p,log = T)
 b.lik_sum=sum(b.lik)
 print(b.lik_sum)
-plot(jitter(bears_presence)~b2pa.distance, xlim=c(0,20),xlab="Distance to Nearest Protected Area (km)",ylab="Reported Conflict Point (Bear = 1, Other = 0)")
+# plot(jitter(bears_presence)~b2pa.distance, xlim=c(0,20),xlab="Distance to Nearest Protected Area (km)",ylab="Reported Conflict Point (Bear = 1, Other = 0)")
 B1 <- glm(bears_presence~b2pa.distance,family="binomial") # Fit the other variables in here, to run them together in this model
 
 

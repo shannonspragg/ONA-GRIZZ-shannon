bbc06dec59e27b7296991e3f6f8fd63c7465f9d1
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-10-14 11:25:33 -0600

    successfully made the social raster!

--- Producing GrizzIncrease Raster.R
+++ Producing GrizzIncrease Raster.R
@@ -11,5 +11,23 @@ library(terra)
 # Here we import the already produced sociobio and combined resist rasters (from the ONA GRIZZ folder):
 sociobio <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/sociobio_resist.tif")
 comb.resist <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/combined_resist.tif")
+plot(sociobio)
+plot(comb.resist)
 
 
+# Raster Math: Subtract to get Social Raster ------------------------------
+# Here I subtract the sociobio raster from the combined resistance raster to get social (grizzIncrease)
+social <- comb.resist - sociobio
+social
+plot(social) # Yep, this shows us the difference - NICE!
+
+
+# Save the New Raster -----------------------------------------------------
+# Let's write this as a tif file and save it for use in omniscape
+
+writeRaster(social, "/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/GrizzIncrease (Social).tif")
+
+# Let's confirm this saved properly:
+social.test <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/GrizzIncrease (Social).tif")
+plot(social.test)
+social.test # Checked, this saved properly!

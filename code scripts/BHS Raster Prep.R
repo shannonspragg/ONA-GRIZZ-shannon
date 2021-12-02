655bffb0d2f58aae4cb7b07852df32c982c5cc56
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-12-02 14:50:03 -0700

    moving these

--- code scripts/BHS Raster Prep.R
+++
@@ -1,7 +0,0 @@
-# Prepping the BHS Raster from Bear Desnity PNAS --------------------------
-# Here I will be pulling the NDVI, HII, and CC Datasets and checking the scale, 
-# multiplying them by the parameter estimates in the paper, and then stacking them
-# into one final "bear density" raster
-
-
-

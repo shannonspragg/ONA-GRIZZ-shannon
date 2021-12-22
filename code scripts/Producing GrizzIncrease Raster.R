db2e1d90325589369b8743ed466f7f6fbb58b7ba
Shannon Spragg <shannonspragg@u.boisestate.edu>
2021-12-22 10:19:33 -0800

    fixing this -- I think -- to the proper grizzinc raster

--- code scripts/Producing GrizzIncrease Raster.R
+++ code scripts/Producing GrizzIncrease Raster.R
@@ -1,6 +1,7 @@
 # Prepping Survey (GrizzIncrease)Raster (from Combined Resist & Sociobio) ---------------
 # Here I will use raster math to subtract the sociobio raster from the combined resistance,
 # to produce the difference which represents the purely social layer, the GrizzIncrease raster.
+## UPDATE: should be subtracting the biophys from sociobio
 
 
 # Load Packages -----------------------------------------------------------
@@ -11,28 +12,38 @@ library(terra)
 # Here we import the already produced sociobio and combined resist rasters (from the ONA GRIZZ folder):
 sociobio <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/sociobio_resist.tif")
 comb.resist <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/bio_combined_resist.tif")
+
+# Bring in the biophys only raster:
+biophys <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/biophys_normalized_cum_currmap.tif")
 plot(sociobio)
 plot(comb.resist)
-
+plot(biophys)
 
 # Raster Math: Subtract to get Social Raster ------------------------------
 # Here I subtract the sociobio raster from the combined resistance raster to get social (grizzIncrease)
-social <- comb.resist - sociobio # This produces negative resistances... let's try to flip it
 social.2 <- sociobio - comb.resist # this gives us the difference - which is the + grizz inc
 social.2
 plot(social.2) # I think this shows us the difference - NICE!
 
+# REVISED: do this by subtracting the biophys from sociobio (which is biophys + 1 support for grizzinc)
+grizzinc <- sociobio - biophys
+plot(grizzinc)
 
 # Save the New Raster -----------------------------------------------------
 # Let's write this as a tif file and save it for use in omniscape
 
 writeRaster(social.2, "/Users/shannonspragg/rasters/GrizzIncrease (Social)_2.tif")
 
+writeRaster(grizzinc, "/Users/shannonspragg/rasters/GrizzIncrease Raster.tif")
+
 # Let's confirm this saved properly:
 social.test <- rast("/Users/shannonspragg/ONA_GRIZZ/Sociobio, Resist, & Survey Rasters/GrizzIncrease (Social)_2.tif")
 plot(social.test)
 social.test # Checked, this saved properly!
 
+grizzinc.test <- rast("/Users/shannonspragg/rasters/GrizzIncrease Raster.tif")
+plot(grizzinc.test)
+grizzinc.test # Checked, this saved properly!
 
 # Bring in Grizz Density Raster -------------------------------------------
 # Bringing this in to check the dimensions and projection:

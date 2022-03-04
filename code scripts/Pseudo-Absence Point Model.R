02bcc2ba1bdc1cf41eb02078645fe0df8f15f1b6
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-03-04 16:17:30 -0700

    trying

--- code scripts/Pseudo-Absence Point Model.R
+++ code scripts/Pseudo-Absence Point Model.R
@@ -32,5 +32,37 @@ p.abs.pts <- randomPoints(soi.rast, 4000)
 plot(soi.rast)
 plot(p.abs.pts, add=TRUE) # This gives us our absence points!
 
+# Make this a data frame:
+abs.pts.df <- data.frame(p.abs.pts)
+
+
+################## START HERE: need to merge dataframes together despite missing columns
+
+# Make these spatial points:
+abs.pts.sf <- st_as_sf(abs.pts.df, coords= c("x","y"), crs= st_crs(warp.ccs.df))
+
+# Add the missing columns:
+add.cols <- setdiff(names(warp.ccs.df), names(abs.pts.sf))
+
+add_columns(abs.pts.sf, bears.reps)
+
+
+
+abs.pts.sf.b <- cbind(abs.pts.sf, add.cols)
+
+
+# Restructure Dataframe: --------------------------------------------------
+  #### Here I need to separate out the bear-only reports as their own data frame, then add the absence points in - probably have to re-extract all of these.
+
+bears.reps <- warp.ccs.df %>% filter(warp.ccs.df$bears == "1")
+
+# Join our bear points with the absence points:
+
+bear.pts.w.abs <- dplyr::full_join(bears.reps, abs.pts.sf)
+
+
+
+
+
 
 

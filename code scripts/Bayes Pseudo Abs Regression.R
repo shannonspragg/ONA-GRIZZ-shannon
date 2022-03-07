b99ce73f862c6c472bbc23fa34bdebff78820a64
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-03-06 16:31:38 -0800

    starting this

---
+++ code scripts/Bayes Pseudo Abs Regression.R
@@ -0,0 +1,14 @@
+# Bayes Regression: Pseudo - Abs Model: -----------------------------------
+    ## Here we run a simple Bayes regression using our WARP pseudo-absence points instead of the species vs bears
+
+
+# Load Packages: ----------------------------------------------------------
+
+
+
+# Import Data: ------------------------------------------------------------
+
+warp.pres.abs <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP Cropped - SIP/warp_presabs_hum_dens.shp")
+str(warp.pres.abs)
+
+

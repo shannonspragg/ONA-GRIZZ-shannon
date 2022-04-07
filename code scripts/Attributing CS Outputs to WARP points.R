a994222003bf90a9464f949781b93328b5b5702b
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-04-07 15:03:22 -0600

    edits, dont need this

--- code scripts/Attributing CS Outputs to WARP points.R
+++ code scripts/Attributing CS Outputs to WARP points.R
@@ -1,15 +1,10 @@
 # Overlaying WARP Points with CS Outputs ----------------------------------
-# In this script I will be bringing in the produced CS's (grizzinc and biophysical)
-# and overlaying each individual one with the WARP points, buffering the points by 500m, and
-# then extracting the attributes from each raster to each WARP point by location. The result should
-# be the creation of three additional columns (one for each CS) in the master WARP df, representing these values.
+  ## Here we bring in the predictor rasters and overlay each individual one with the WARP points, buffering the points by 500m, and
+  # then extract the attributes from each raster to each WARP point by location. The result should be the creation of three additional 
+  # columns (one for each raster) in the master WARP df, representing these values.
 
-##### NOTE: If we want to estimate the effects of opinions on conflict, we need to extract them directly from the resistance raster 
-# If we want to know how opinions might mix with biology to affect movement, we would want to extract from omniscape results 
-# based on the sociobio resistance surface
-
-# Should have: extracted values from BHS (not cs, just the grizz_density), a sociobio CS (social + biophys), Biophysical CS (see biophys_resist) in ONA folder, 
-# and plain griz_resist survey resistance values (not cs, extracted from normal raster).
+  # Should have: extracted values from BHS (the grizz_density estimate), Biophysical CS ( biophys raster) , 
+  # and  griz_inc survey values (not cs, extracted from normal raster).
 
 # Load Packages -----------------------------------------------------------
 library(sf)
@@ -20,147 +15,136 @@ library(sp)
 library(terra)
 library(rgdal)
 
-# Bring in WARP Master df and CS Rasters ----------------------------------
-warp.all.sp <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP Cropped - SIP/warp_pres.abs.shp")
-
+# Bring in Data: ----------------------------------
+  # Our WARP points master df:
+warp.all.sp <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /warp.master.shp")
+  # Our pres-abs ponts master df:
+pres.abs.master <- ist_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /pres.abs.master.shp")
  
-# CS's for sociobio and biophysical:
+# Predictor Rasters:
 biophys.cum.curmap <- rast("/Users/shannonspragg/ONA_grizz_Matt/data/processed/output/biophys_CS/cum_currmap.tif") # use this one
 # Cumulative current flow shows the total current for each landscape pixel
-
-biophys.norm.cum.curmap <- rast("/Users/shannonspragg/ONA_grizz_Matt/data/processed/output/biophys_CS/normalized_cum_currmap.tif") 
 # Normalized shows the degree to which a pixel has more or less current than expected under resistance-free conditions (cumulative current flow divided by flow potential)
 
-#sociobio.cum.curmap <- rast("") # NEED TO UPDATE (later)
-
 # Just survey response layer (not CS):
-grizz.inc.raster <- rast("/Users/shannonspragg/rasters/grizz_inc_BC.tif") #  the proportion of people within statscan census that 
+grizz.inc.raster <- rast("/Users/shannonspragg/rasters/grizz_inc.tif") #  the proportion of people within a census that 
 # responded “I would like to see grizzlies increase or increase substantially” in response to “how would you like to see grizzly 
 # populations respond in the next several years?” 
 
 # BHS layer:
-grizz.dens <- rast("/Users/shannonspragg/ONA_GRIZZ/Grizz Density rasters/grizz_dens.tif")
+grizz.dens <- rast("/Users/shannonspragg/ONA_GRIZZ/Grizz Density rasters/grizz_dens.tif") # Estimated grizzly density for the region
 plot(grizz.dens)
 
+  # SOI Boundary and Raster for template:
 soi.10k.boundary <- st_read("/Users/shannonspragg/ONA_GRIZZ/CAN Spatial Data/SOI Ecoprovince Boundary/SOI_10km_buf.shp")
 
-# Bring in Provinces and Filter to BC (skip to read in) -------------------------------------
-can.prov <- st_read("/Users/shannonspragg/ONA_GRIZZ/CAN Spatial Data/CAN Province Boundaries/lpr_000b16a_e.shp")
-bc.boundary <- can.prov %>% 
-  filter(., PRNAME == "British Columbia / Colombie-Britannique") %>% 
-  st_make_valid()
-bc.reproj <- st_transform(bc.boundary, st_crs(warp.all.sp))
-plot(st_geometry(bc.reproj))
-st_write(bc.reproj, "/Users/shannonspragg/ONA_GRIZZ/CAN Spatial Data/BC Boundary.shp")
-
-# Read this in for future runs:
-bc.reproj <- st_read("/Users/shannonspragg/ONA_GRIZZ/CAN Spatial Data/BC Boundary.shp")
-
-
-
-# Crop WARP Points to within the BC Boundary (skip this) ------------------------------
-plot(st_geometry(warp.all.sp))
-warp.all.sp.bc <- st_crop(warp.all.sp, bc.reproj)
-plot(st_geometry(warp.all.sp.bc)) # Cropped these to only the points in BC
-st_crs(warp.all.sp.bc)
-crs(warp.all.sp.bc)
-
-st_write(warp.all.sp.bc, "/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP All Species Full Yr/ WARP All Sp Full Yr BC.shp")
-
-# Read this in for future runs:
-warp.all.sp.bc <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /WARP All Species Full Yr/ WARP All Sp Full Yr BC.shp")
-
+soi.rast <- terra::rast("/Users/shannonspragg/ONA_GRIZZ/CAN Spatial Data/SOI Ecoprovince Boundary/SOI_10km.tif") # SOI Region 10km buffer raster
 
 
 # Check / Set CRS for Raster and Points -----------------------------------
  # Match the projection and CRS of the current map to the resistance maps:
  # Here we use the grizz.inc raster to be the template crs
 
-    # Bear Density (BHS) Resistance Map:
-crs(grizz.dens) <- crs(grizz.inc.raster) 
-crs(grizz.inc.raster) == crs(grizz.dens) # Nice, this worked --> now in BC Albers EPSG 3005
-    # Grizzinc Survey +1 Grizz Resistance Map:
-crs(biophys.cum.curmap) <- crs(grizz.inc.raster) 
-crs(biophys.cum.curmap) == crs(grizz.inc.raster) # Nice, this worked --> now in BC Albers EPSG 3005
-    
+    # Bear Density (BHS) Estimate:
+crs(grizz.dens) <- crs(soi.rast) 
+crs(grizz.inc.raster) == crs(soi.rast) # Nice, this worked --> now in BC Albers EPSG 3005
+    # Biophys Map:
+crs(biophys.cum.curmap) <- crs(soi.rast) 
+crs(biophys.cum.curmap) == crs(soi.rast) # Nice, this worked --> now in BC Albers EPSG 3005
+  # GrizzInc Map:
+crs(grizz.inc.raster) <- crs(soi.rast) 
+crs(grizzinc.raster) == crs(soi.rast) # Nice, this worked --> now in BC Albers EPSG 3005
 
-# Project BC boundary:
+
+  # Project SOI boundary:
 soi.reproj <- st_make_valid(soi.10k.boundary) %>% 
   st_transform(crs=crs(grizz.inc.raster))
 
-# Match the projection and CRS of the WARP to the resistance map:
 st_crs(warp.all.sp) # This is in NAD83 BC Albers - EPSG 3005
 
-# Match the sf points CRS directly to the resistance raster:
+  # Match the sf points CRS directly to the resistance raster:
 warp.reproj <- st_make_valid(warp.all.sp) %>% 
-  st_transform(crs=crs(grizz.inc.raster))
+  st_transform(crs=crs(soi.rast))
   
-plot(st_geometry(warp.reproj))
+pres.abs.reproj <- st_make_valid(pres.abs.master) %>% 
+  st_transform(crs=crs(soi.rast))
+plot(st_geometry(pres.abs.reproj))
+st_crs(pres.abs.reproj)
 st_crs(warp.reproj)
-crs(grizz.inc.raster) # The same as above, just formatted differently - success!
+crs(soi.rast) # The same as above, just formatted differently - success!
 
-# Check Raster Resolutions:
+  # Check Raster Resolutions:
 str(grizz.dens)
 res(biophys.cum.curmap) # 1000 x 1000
 res(grizz.inc.raster) # 1000 x 1000
-#res(sociobio.cum.curmap)
+res(soi.rast)
 
-# Need to make points a SpatVector:
-warp.sv <- vect(warp.reproj)
-str(warp.sv)
-crs(warp.sv)
-
-# Plot them together to see if projection is same:
-plot(grizz.inc.raster)
-plot(warp.sv, add = TRUE) # HECK YES
-
-plot(grizz.dens)
-plot(warp.sv, add = TRUE) # Sweet
-
-#plot(sociobio.cum.curmap)
-#plot(warp.sv, add = TRUE) # Againnn
-
-plot(biophys.cum.curmap)
-plot(warp.sv, add = TRUE) # AGAIN FOR THE PEOPLE IN THE BACK
 
 # Buffer the WARP Points (Before Overlay) --------------------------------------------------
-# Here we buffer the WARP points by 5km before extracting the attributes from the current maps
+  # Here we buffer the WARP and ppres-abs points by 5km before extracting the attributes from the current maps
 warp.all.buf <- warp.reproj %>% 
   st_buffer(., 5000)
 plot(st_geometry(warp.all.buf)) # Check the buffers
 
-st_crs(warp.all.buf)
+pres.abs.buf <- pres.abs.reproj %>% 
+  st_buffer(., 5000)
+plot(st_geometry(pres.abs.buf)) # Check the buffers
 
-# Let's Turn the Buffered Points into a SpatVector:
+
+  # Let's Turn the Buffered Points into a SpatVector:
 warp.sv.buf <- vect(warp.all.buf)
-crs(warp.sv.buf)
-str(warp.sv.buf)
-plot(warp.sv.buf)
+pres.abs.sv.buf <- vect(pres.abs.buf)
 
-# Overlay WARP Points with CS Raster BUFFERED --------------------------------------
-# Here I will extract the mean values from each raster to the buffered points
+  # Plot them together to see if projection truly is same:
+plot(grizz.inc.raster)
+plot(warp.sv.buf, add = TRUE) 
 
-warp.biophys.b.ext <- terra::extract(biophys.cum.curmap, warp.sv.buf, mean, na.rm = TRUE) 
-# This gives us the mean value of each buffered area --> what we want!
+plot(grizz.dens)
+plot(warp.sv.buf, add = TRUE) 
+
+plot(soi.rast)
+plot(warp.sv.buf, add = TRUE) 
+
+plot(biophys.cum.curmap)
+plot(warp.sv.buf, add = TRUE) 
+
+plot(soi.rast)
+plot(pres.abs.sv.buf, add= TRUE)
+
+# Overlay WARP Points with CS Raster  --------------------------------------
+  # Here we extract the mean values from each raster to the buffered points
+warp.biophys.b.ext <- terra::extract(biophys.cum.curmap, warp.sv.buf, mean, na.rm = TRUE)  # This gives us the mean value of each buffered area --> what we want!
 warp.grizz.inc.b.ext <- terra::extract(grizz.inc.raster, warp.sv.buf, mean, na.rm = TRUE) 
 warp.bhs.b.extract <- terra::extract(grizz.dens, warp.sv.buf, mean, na.rm = TRUE) 
 
-# Create New Column(s) for Extracted Values:
-warp.reproj$BphysEx <- warp.biophys.b.ext[,2]
-#warp.reproj$SociobioExtract <- warp.sociobio.b.ext[,2]
+pres.abs.biophys.b.ext <- terra::extract(biophys.cum.curmap, pres.abs.sv.buf, mean, na.rm = TRUE)  # This gives us the mean value of each buffered area --> what we want!
+pres.abs.grizz.inc.b.ext <- terra::extract(grizz.inc.raster, pres.abs.sv.buf, mean, na.rm = TRUE) 
+pres.abs.bhs.b.extract <- terra::extract(grizz.dens, pres.abs.sv.buf, mean, na.rm = TRUE) 
+
+  # Create New Column(s) for Extracted Values:
+warp.reproj$BphysEx <- warp.biophys.b.ext[,2]   # MAke sure col names match!
 warp.reproj$GrzzInE <- warp.grizz.inc.b.ext[,2]
 warp.reproj$BHSExtr <- warp.bhs.b.extract[,2]
+
+pres.abs.reproj$BphysEx <- pres.abs.biophys.b.ext[,2]
+pres.abs.reproj$GrzzInE <- pres.abs.grizz.inc.b.ext[,2]
+pres.abs.reproj$BHSExtr <- pres.abs.bhs.b.extract[,2]
 
 # Check for NA's:
 which(is.na(warp.reproj$BphysEx)) #none
 which(is.na(warp.reproj$BHSExtr)) #none
-which(is.na(warp.reproj$GrzzInE)) # none!
+which(is.na(warp.reproj$GrzzInE)) # none
+which(is.na(pres.abs.reproj$BphysEx)) #none
+which(is.na(pres.abs.reproj$BHSExtr)) #none
+which(is.na(pres.abs.reproj$GrzzInE)) # none
 
 
 # Save this as new file ---------------------------------------------------
 
-st_write(warp.reproj, "/Users/shannonspragg/ONA_GRIZZ/WARP Bears /warp.need.farms.shp")
-warp.reproj <- st_read("/Users/shannonspragg/ONA_GRIZZ/WARP Bears /updated.master.df.shp")
+st_write(warp.reproj, "/Users/shannonspragg/ONA_GRIZZ/WARP Bears /warp.final.shp")
+st_write(pres.abs.reproj, "/Users/shannonspragg/ONA_GRIZZ/WARP Bears /pres.abs.final.shp")
+
+
 
 # Check for NA's: ---------------------------------------------------------
 which(is.na(warp.reproj$BphysEx)) # We have about 70 NA's here..
